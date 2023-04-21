a5b5adb8d3e590616ad0b619620d4319a19ed5e8
FahadAlAraik <51764194+FahadAlAraik@users.noreply.github.com>
2023-04-21 23:53:27 +0300

    Update route.tsx

--- route.tsx
+++ route.tsx
@@ -2,13 +2,28 @@ import * as jose from 'jose'
 
 export async function POST(req:Request,res:NextApiResponse) {
 
+                             
+    // assume that I query here for user data
+    
+    const user = prisma.user.findFirst({
+        where:{
+            email:{
+                equals:'test@gmail.com'
+            }
+        },
+        select: {
+            email:true    
+        }
+        
+    })
+    
     const alg = "HS256" // algorithm in header
     const secret = new TextEncoder().encode(process.env.JWT_secret)
     const token = await new jose.SignJWT({'email':'test@gmail.com'})
                                  .setProtectedHeader({alg})
                                  .setExpirationTime('24h')
                                  .sign(secret)
-                                 
+    
     const respo = new NextResponse()
     respo.cookies.set('jwt',token)
     return NextResponse.json({user},{headers:respo.headers})
